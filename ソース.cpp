6cf1b95d322b6d0543c148dbdcee8023a78791f0
Satori32 <Huga8989+githubf@gmail.com>
2023-11-05 11:06:12 +0900

    プロジェクト ファイルを追加します。

---
+++ ソース.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <tuple>
+#include <cstdint>
+#include <string>
+#include <vector>
+
+struct SystemTag {
+	struct BasicInfo
+	{
+		std::uint64_t ID=0xdeadbeef;
+		double Version = 0.25;
+	} BasicInfo;
+	
+	std::tuple<std::vector<std::uint64_t>, std::uint64_t> ExtraInfo;//if you need the extention to info. yes you can count up to tuples count. but i am confuse.
+
+};
+
+int main() {
+	SystemTag S;
+
+	S.BasicInfo.ID = 0;
+
+	
+}
